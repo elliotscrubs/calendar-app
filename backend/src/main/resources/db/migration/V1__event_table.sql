d0f892327f8099a84baedba78a095af06a0c5298
Ildikó Szabó <szabo.ildiko0101@gmail.com>
2025-02-27 16:33:18 +0100

    create new database and create events_table  (#1)
    * new database, create events table
    
    * .
    
    * primary key, events table update
    
    ---------
    
    Co-authored-by: Ildikó Szabó <szab.ildiko89@gmail.com>

--- backend/src/main/resources/db/migration/V1__event_table.sql
+++ backend/src/main/resources/db/migration/V1__event_table.sql
@@ -1,2 +1,7 @@
--- NOOP migration
-SELECT 1;
+create table events (
+    id binary(16) default (uuid()) primary key,
+    user_id binary(16) not null,
+    start_at datetime not null,
+    end_at datetime not null,
+    event_text text(500) not null
+)
