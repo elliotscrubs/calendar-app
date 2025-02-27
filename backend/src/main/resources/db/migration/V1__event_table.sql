4d52a8b2aedce783afc1b76660a2b1de31eef909
Ildikó Szabó <szab.ildiko89@gmail.com>
2025-02-27 16:30:48 +0100

    primary key, events table update

--- backend/src/main/resources/db/migration/V1__event_table.sql
+++ backend/src/main/resources/db/migration/V1__event_table.sql
@@ -1,6 +1,7 @@
-CREATE TABLE `events_table` (
-    userId int,
-    start_at TIME,
-    end_at TIME,
-    event text(500)  
+create table events (
+    id binary(16) default (uuid()) primary key,
+    user_id binary(16) not null,
+    start_at datetime not null,
+    end_at datetime not null,
+    event_text text(500) not null
 )
