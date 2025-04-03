a899eaafecf262d5257bcdbd0ef7a74c0e7da98c
Ildikó Szabó <szabo.ildiko0101@gmail.com>
2025-04-03 09:06:46 +0200

    Event list endpoint (#5)
    * Update EventController.java
    
    * Update EventController.java
    
    * event lists byDate
    
    * Update EventController.java
    
    * fixed
    
    ---------
    
    Co-authored-by: Ildikó Szabó <szab.ildiko89@gmail.com>

--- backend/src/main/java/com/ildikoszabo/calendar_app/controller/EventController.java
+++ backend/src/main/java/com/ildikoszabo/calendar_app/controller/EventController.java
@@ -3,9 +3,14 @@ package com.ildikoszabo.calendar_app.controller;
 import com.ildikoszabo.calendar_app.entity.Event;
 import com.ildikoszabo.calendar_app.service.EventService;
 import jakarta.validation.Valid;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 
 @RestController
@@ -30,5 +35,14 @@ public class EventController {
     @RequestMapping(method = RequestMethod.OPTIONS)
     public void handleOptions() {
 
+    }
+
+    @GetMapping("/byDate")
+    public ResponseEntity<Map<LocalDate, List<Event>>> getListByDate(
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate fromDate,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate toDate,
+            @RequestParam UUID userId) {
+        Map<LocalDate, List<Event>> allEvents = eventService.getListByDate(fromDate, toDate, userId);
+        return ResponseEntity.ok(allEvents);
     }
 }
--- backend/src/main/java/com/ildikoszabo/calendar_app/repository/EventRepository.java
+++ backend/src/main/java/com/ildikoszabo/calendar_app/repository/EventRepository.java
@@ -2,11 +2,17 @@ package com.ildikoszabo.calendar_app.repository;
 
 import com.ildikoszabo.calendar_app.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.math.BigInteger;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, BigInteger> {
-
+	@Query("SELECT e FROM Event e WHERE startAt BETWEEN :fromDate AND :toDate")
+	List<Event> findByStartAtAndEndAtAndUserId(@Param("fromDate") LocalDateTime fromDate,
+											   @Param("toDate") LocalDateTime toDate, UUID userId);
 }
