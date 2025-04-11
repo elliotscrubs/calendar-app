b70c3f380ad6f87749eb86ee37c0c8d3d920ee31
Ildikó Szabó <szab.ildiko89@gmail.com>
2025-04-11 12:44:28 +0200

    call byDate endpoint, calendarClient

--- frontend/src/app/api/calendarClient.tsx
+++ frontend/src/app/api/calendarClient.tsx
@@ -1,37 +1,56 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosInstance } from 'axios';
 type UUID = string;
 
 export interface CreateEventRequest {
-    userId: UUID;
-    startAt: Date;
-    endAt: Date;
-    eventText: string;
+  userId: UUID;
+  startAt: Date;
+  endAt: Date;
+  eventText: string;
 }
 
 export interface Event {
-    id: UUID;
-    userId: UUID;
-    startAt: Date;
-    endAt: Date;
-    eventText: string;
+  id: UUID;
+  userId: UUID;
+  startAt: Date;
+  endAt: Date;
+  eventText: string;
+}
+
+export type ByDateResponse = Record<string, Event[]>;
+
+function formatDate(date: Date): string {
+  const offset = date.getTimezoneOffset()
+  const offsetApplied = new Date(date.getTime() - (offset*60*1000))
+  return offsetApplied.toISOString().split('T')[0]
 }
 
 class CalendarClient {
-    private axiosInstance: AxiosInstance;
+  private axiosInstance: AxiosInstance;
 
-    constructor(baseURL: string) {
-        this.axiosInstance = axios.create({
-          baseURL,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-    }   
+  constructor(baseURL: string) {
+    this.axiosInstance = axios.create({
+      baseURL,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
 
-    async createEvent(data: CreateEventRequest): Promise<Event> {
-        const response = await this.axiosInstance.post<Event>("/events", data);
-        return response.data;
-    }
+  async createEvent(data: CreateEventRequest): Promise<Event> {
+    const response = await this.axiosInstance.post<Event>('/events', data);
+    return response.data;
+  }  
+
+  async displayEvents(userId: UUID, fromDate: Date, toDate: Date) {
+    const response = await this.axiosInstance.get<ByDateResponse>('/events/byDate', {
+      params: {        
+        userId: userId,
+        fromDate: formatDate(fromDate),
+        toDate: formatDate(toDate)
+      },
+    });
+    return response.data;
+  }
 }
 
 export const calendarClient = new CalendarClient('http://localhost:9090');
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,24 +1,57 @@
 'use client';
-import React from "react";
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
+import React from 'react';
+import { calendarClient } from './api/calendarClient';
 
-const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from '@mui/material';
+
+const weekdays = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
 
 const cellStyle = {
-  fontWeight: "bold",
-  color: "white",
-  backgroundColor: "#1976d2",
-  borderRight: "1px solid white", // elválasztó vonal
+  fontWeight: 'bold',
+  color: 'white',
+  backgroundColor: '#1976d2',
+  borderRight: '1px solid white',
 };
 
 const EventsTable = () => {
+  const eventTest = calendarClient.displayEvents("00000300-0000-0000-0000-000000000000", new Date("2025-03-13"), new Date("2025-04-13"));
+  console.log(eventTest);
+
+  
   return (
-    <TableContainer component={Paper} sx={{ maxWidth: 1500, margin: "auto", borderRadius: 3, mt: 3  }}>
+    <TableContainer
+      component={Paper}
+      sx={{ maxWidth: 1500, margin: 'auto', borderRadius: 3, mt: 3 }}>
       <Table>
         <TableHead>
           <TableRow>
             {weekdays.map((day, index) => (
-              <TableCell key={index} align="center" style={{ fontWeight: "bold", color: "white", backgroundColor: "#1976d2", borderRight: "1px solid white" }}>
+              <TableCell
+                key={index}
+                align='center'
+                style={{
+                  ...cellStyle,
+                  borderRight:
+                    index === weekdays.length - 1
+                      ? 'none'
+                      : cellStyle.borderRight,
+                }}>
                 {day}
               </TableCell>
             ))}
@@ -26,8 +59,14 @@ const EventsTable = () => {
         </TableHead>
         <TableBody>
           <TableRow>
-            {weekdays.map((_, index) => (
-              <TableCell key={index} align="center"></TableCell>
+            {weekdays.map((dayName, index) => (
+              <TableCell
+                key={index}
+                align='center'
+                style={{
+                  borderRight:
+                    index === weekdays.length - 1 ? 'none' : '1px solid #ccc',
+                }}></TableCell>
             ))}
           </TableRow>
         </TableBody>
@@ -37,5 +76,3 @@ const EventsTable = () => {
 };
 
 export default EventsTable;
-
-
