693fde0380fc7f177f2872f57d533baeb9bf43bc
Ildikó Szabó <szab.ildiko89@gmail.com>
2025-05-15 10:17:03 +0200

    update DayCell.tsx

--- frontend/src/app/components/DayCell.tsx
+++ frontend/src/app/components/DayCell.tsx
@@ -1,20 +1,15 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { Event } from '../api/calendarClient';
+import { useMemo } from 'react';
 import { ByDateResponse } from '../api/calendarClient';
 import EventCard from './EventCard';
 
 const DayCell = (props: { index: number; events: ByDateResponse }) => {
-  const [eventsList, setEventsList] = useState<Event[]>([]);
-
-  useEffect(() => {
-    const newEventList = Object.keys(props.events)
+  const eventsList = useMemo(() => {
+    return Object.keys(props.events)
       .filter(date => (props.index + 1) % 7 == new Date(date).getDay())
       .map(date => props.events[date])
       .flat();
-
-    setEventsList(newEventList);
   }, [props.index, props.events]);
 
   return eventsList.map((events, index) => (
