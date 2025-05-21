db998b90d926a4a0f2b5f211f0f170764b1f789f
Ildikó Szabó <szab.ildiko89@gmail.com>
2025-05-21 11:52:02 +0200

    create delete endpoint
    and testing

--- backend/src/main/java/com/ildikoszabo/calendar_app/service/EventService.java
+++ backend/src/main/java/com/ildikoszabo/calendar_app/service/EventService.java
@@ -1,13 +1,15 @@
 package com.ildikoszabo.calendar_app.service;
 
 import com.ildikoszabo.calendar_app.entity.Event;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface EventService {
    Event save(Event theEvent);
    Map<LocalDate, List<Event>> getListByDate(LocalDate fromDate, LocalDate toDate, UUID userId);
+   void deleteById(UUID id);
+   Optional<Event> findById(UUID id);
 }
