77f7545a07f81b4558a7f9a1b5526f4ed42ee411
elliotscrubs <szabo.ildiko0101@gmail.com>
2025-10-30 14:54:50 +0100

    .

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+	output: 'export',
   reactStrictMode: true,
   webpack: (config) => { // webpack cache hibauzenet (keves memoria vagy tul nagy cache fajlok miatt)
     config.cache = false; 
