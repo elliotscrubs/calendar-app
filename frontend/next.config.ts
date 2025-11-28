e8d175cd273fa2f5ccdb4365c5b4fbecdfdc5388
Ildikó Szabó <szabo.ildiko0101@gmail.com>
2025-11-28 09:42:20 +0100

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-	output: 'export',
+	// output: 'export',
   reactStrictMode: true,
-  webpack: (config) => { // webpack cache hibauzenet (keves memoria vagy tul nagy cache fajlok miatt)
+  webpack: (config) => { 
     config.cache = false; 
     return config;
   }
 };
-
 
 export default nextConfig;
